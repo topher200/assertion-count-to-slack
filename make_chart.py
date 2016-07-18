f1f0a25e2498641cd086cc505c4e8495bc385ca3
Topher Brown <t.brown@wordstream.com>
2016-07-18 11:13:47 -0400

    run through make_chart with testing data

--- make_chart.py
+++ make_chart.py
@@ -17,12 +17,33 @@ def make_chart(_):
     request_data = {
         'template': 'assertion-count-timeseries',
         'options': {
-            'columns': [
-                [1,2,3,4],
-                [5,6,7,8],
-            ]
+            'data': {
+                'columns': [
+                    ["x",
+                     "2013-01-01",
+                     "2013-01-02",
+                     "2013-01-03",
+                     "2013-01-04",
+                     "2013-01-05",
+                     "2013-01-06"],
+                    ['data1', 1, 2, 3, 4],
+                    ['data2', 5, 6, 7, 8],
+                ]
+            }
         }
     }
-    res = requests.post(CHART_REQUEST_URL % API_KEY, request_data)
-    print res
+
+    request_url = CHART_REQUEST_URL % API_KEY
+    print 'making call to %s with %s' % (request_url, request_data)
+    res = requests.post(request_url, json=request_data)
+    if not res.ok:
+        print 'request failed'
+        print res.content
+
+    chart_url = res.json()['short_url']
+    print 'chart_url: ', chart_url
+
+
+if __name__ == '__main__':
+    make_chart(None)
 
